733afe24a4f0a4b8d15e7d68cf3a1725eecf9dfb
Gertrk <gert.rk1@hotmail.com>
2014-04-15 10:07:55 +0300

    Add Halo files to pm

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+// Project constants
+define('PROJECT_NAME', 'pm');
+define('DEFAULT_CONTROLLER', 'Newsarticles');
+define('DEBUG', false);
+
+// Load app
+require 'system/classes/Application.php';
+$app = new Application;
---
+++ views/Newsarticles/Newsarticles_index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gert
+ * Date: 14.04.14
+ * Time: 15:51
+ */ 
